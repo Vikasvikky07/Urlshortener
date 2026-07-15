77e1442d940d23835997274260448e5056f545ce
vikas <vikasvicky12v@gmail.com>
2026-07-15 22:32:52 +0530

    Added openAPI documentation

---
+++ src/main/java/org/paytm/urlshortener/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package org.paytm.urlshortener.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI urlShortenerOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("URL Shortener API")
+                        .description("API for creating short URLs and redirecting short codes to original URLs.")
+                        .version("1.0.0"));
+    }
+}
